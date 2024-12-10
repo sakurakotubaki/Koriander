da82ca9e449b77b1fb3eb22a98b7c356641f56fb
Junichi Hashimoto <73347485+sakurakotubaki@users.noreply.github.com>
2024-12-10 23:33:38 +0900

    コリアンダーアプリ (#1) (#2)
    * added: python library
    
    * feat: crud cors
    
    * docs: pj info

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from routers import koriander
 
 app = FastAPI()
 
+# CORSミドルウェアを追加
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 本番環境では具体的なオリジンを指定することを推奨
+    allow_credentials=True,
+    allow_methods=["*"],  # 許可するHTTPメソッド
+    allow_headers=["*"],  # 許可するHTTPヘッダー
+)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+app.include_router(koriander.router)
